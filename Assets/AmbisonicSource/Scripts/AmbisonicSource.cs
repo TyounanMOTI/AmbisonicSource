6780c58331fb7dbc578ee2691a3327f5d23abe00
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2017-04-29 02:55:17 +0900

    HoloLens向けシーンを作成

---
+++ Assets/AmbisonicSource/Scripts/AmbisonicSource.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AmbisonicSource : MonoBehaviour {
+    [SerializeField]
+    AudioClip _clip;
+    public AudioClip clip {
+        get { return _clip; }
+        private set {
+            _clip = value;
+            LoadClip();
+        }
+    }
+
+    [SerializeField]
+    AudioMixerGroup _output;
+    public AudioMixerGroup output {
+        get { return _output; }
+        set {
+            _output = value;
+            speaker.SetOutput(value);
+        }
+    }
+
+    [SerializeField]
+    bool _mute;
+    public bool mute {
+        get { return _mute; }
+        set {
+            _mute = value;
+            speaker.SetMute(value);
+        }
+    }
+
+    [SerializeField]
+    bool _bypassEffects;
+    public bool bypassEffects {
+        get { return _bypassEffects; }
+        set {
+            _bypassEffects = value;
+            speaker.SetBypassEffects(value);
+        }
+    }
+
+    [SerializeField]
+    bool _bypassListenerEffects;
+    public bool bypassListenerEffects {
+        get { return _bypassListenerEffects; }
+        set {
+            _bypassListenerEffects = value;
+            speaker.SetBypassListenerEffects(value);
+        }
+    }
+
+    [SerializeField]
+    bool _bypassReverbZones;
+    public bool bypassReverbZones {
+        get { return _bypassReverbZones; }
+        set {
+            _bypassReverbZones = value;
+            speaker.SetBypassReverbZones(value);
+        }
+    }
+
+    [SerializeField]
+    bool _playOnAwake = true;
+    public bool playOnAwake {
+        get { return _playOnAwake; }
+        set {
+            _playOnAwake = value;
+            speaker.SetPlayOnAwake(value);
+        }
+    }
+
+    [SerializeField]
+    bool _loop;
+    public bool loop {
+        get { return _loop; }
+        set {
+            _loop = value;
+            speaker.SetLoop(value);
+        }
+    }
+
+    [SerializeField, Range(0, 256)]
+    int _priority;
+    public int priority {
+        get { return _priority; }
+        set {
+            _priority = value;
+            speaker.SetPriority(value);
+        }
+    }
+
+    [SerializeField, Range(0.0f, 1.0f)]
+    float _volume = 1.0f;
+    public float volume {
+        get { return _volume; }
+        set {
+            _volume = value;
+            speaker.SetVolume(value);
+        }
+    }
+
+    [SerializeField, Range(-3.0f, 3.0f)]
+    float _pitch = 1.0f;
+    public float pitch {
+        get { return _pitch; }
+        set {
+            _pitch = value;
+            speaker.SetPitch(value);
+        }
+    }
+
+    [SerializeField, Range(0.0f, 1.1f)]
+    float _reverbZoneMix = 1.0f;
+    public float reverbZoneMix {
+        get { return _reverbZoneMix; }
+        set {
+            _reverbZoneMix = value;
+            speaker.SetReverbZoneMix(value);
+        }
+    }
+
+    public float time {
+        get { return speaker.GetTime(); }
+        set { speaker.SetTime(value); }
+    }
+
+    public int timeSamples {
+        get { return speaker.GetTimeSamples(); }
+        set { speaker.SetTimeSamples(value); }
+    }
+
+    AmbisonicSpeaker speaker;
+
+    void Start() {
+        var speakerObject = new GameObject("AmbisonicSpeaker", typeof(AmbisonicSpeaker));
+        speakerObject.transform.SetParent(transform);
+        speaker = speakerObject.GetComponent<AmbisonicSpeaker>();
+        speaker.SetupSpeakers();
+        speaker.SetOutput(output);
+        speaker.SetMute(mute);
+        speaker.SetBypassEffects(bypassEffects);
+        speaker.SetBypassListenerEffects(bypassListenerEffects);
+        speaker.SetBypassReverbZones(bypassReverbZones);
+        speaker.SetPlayOnAwake(playOnAwake);
+        speaker.SetLoop(loop);
+        speaker.SetPriority(priority);
+        speaker.SetVolume(volume);
+        speaker.SetPitch(pitch);
+        speaker.SetReverbZoneMix(reverbZoneMix);
+
+        if (clip != null) {
+            LoadClip();
+            if (playOnAwake) {
+                speaker.Play();
+            }
+        }
+    }
+
+    void LoadClip() {
+        if (clip.loadType != AudioClipLoadType.DecompressOnLoad) {
+            Debug.LogError("AmbisonicSource is currently not support DecompressOnLoad.");
+        }
+        var data = new float[clip.samples * clip.channels];
+        clip.GetData(data, 0);
+        speaker.SetData(data, clip.frequency);
+    }
+}
---
+++ Assets/AmbisonicSource/Scripts/AmbisonicSpeaker.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AmbisonicSpeaker : MonoBehaviour {
+    static readonly Vector3[] positions = {
+        new Vector3(0, 0, 5),   // front
+        new Vector3(-5, 0, 0),  // left
+        new Vector3(0, 0, -5),  // back
+        new Vector3(5, 0, 0),   // right
+        new Vector3(0, 5, 0),   // ceiling
+        new Vector3(0, -5, 0)   // underground
+    };
+
+    string[] names = {
+        "Front",
+        "Left",
+        "Back",
+        "Right",
+        "Ceiling",
+        "Underground"
+    };
+
+    static readonly Vector3[] weight =
+    {
+        new Vector3(1.0f, 0.0f, 0.0f),  // front
+        new Vector3(0.0f, 1.0f, 0.0f),  // left
+        new Vector3(-1.0f, 0.0f, 0.0f), // back
+        new Vector3(0.0f, -1.0f, 0.0f), // right
+        new Vector3(0.0f, 0.0f, 1.0f),  // ceiling
+        new Vector3(0.0f, 0.0f, -1.0f)  // underground
+    };
+
+    AudioSource[] audioSources = new AudioSource[6];
+
+    public void SetupSpeakers() {
+        int index = 0;
+        foreach (var position in positions) {
+            var go = new GameObject(names[index], typeof(AudioSource));
+            go.transform.SetParent(transform);
+            go.transform.position = position;
+            audioSources[index] = go.GetComponent<AudioSource>();
+            audioSources[index].spatialize = true;
+            audioSources[index].spatialBlend = 1.0f;
+            audioSources[index].dopplerLevel = 0.0f;
+            index++;
+        }
+    }
+
+    void Update () {
+        transform.position = Camera.main.transform.position;
+    }
+
+    public void SetData(float[] data, int frequency) {
+        int speakerIndex = 0;
+        float[] decoded = new float[data.Length / 4];
+        foreach (var audioSource in audioSources) {
+            Decode(decoded, data, weight[speakerIndex]);
+            audioSource.clip = AudioClip.Create(names[speakerIndex], data.Length / 4, 1, frequency, false);
+            audioSource.clip.SetData(decoded, 0);
+            speakerIndex++;
+        }
+    }
+
+    void Decode(float[] output, float[] data, Vector3 weight) {
+        for (int i = 0; i < output.Length; i++) {
+            output[i] = (data[i * 4]
+                      + data[i * 4 + 3] * weight[0]
+                      + data[i * 4 + 1] * weight[1]
+                      + data[i * 4 + 2] * weight[2]) / 4.0f;
+        }
+    }
+
+    public void SetOutput(AudioMixerGroup output) {
+        foreach (var audioSource in audioSources) {
+            audioSource.outputAudioMixerGroup = output;
+        }
+    }
+
+    public void SetMute(bool mute) {
+        foreach (var audioSource in audioSources) {
+            audioSource.mute = mute;
+        }
+    }
+
+    public void SetBypassEffects(bool bypassEffects) {
+        foreach (var audioSource in audioSources) {
+            audioSource.bypassEffects = bypassEffects;
+        }
+    }
+
+    public void SetBypassListenerEffects(bool bypassListenerEffects) {
+        foreach (var audioSource in audioSources) {
+            audioSource.bypassListenerEffects = bypassListenerEffects;
+        }
+    }
+
+    public void SetBypassReverbZones(bool bypassReverbZones) {
+        foreach (var audioSource in audioSources) {
+            audioSource.bypassReverbZones = bypassReverbZones;
+        }
+    }
+
+    public void SetPlayOnAwake(bool playOnAwake) {
+        foreach (var audioSource in audioSources) {
+            audioSource.playOnAwake = playOnAwake;
+        }
+    }
+
+    public void SetLoop(bool loop) {
+        foreach (var audioSource in audioSources) {
+            audioSource.loop = loop;
+        }
+    }
+
+    public void SetPriority(int priority) {
+        foreach (var audioSource in audioSources) {
+            audioSource.priority = priority;
+        }
+    }
+
+    public void SetVolume(float volume) {
+        foreach (var audioSource in audioSources) {
+            audioSource.volume = volume;
+        }
+    }
+
+    public void SetPitch(float pitch) {
+        foreach (var audioSource in audioSources) {
+            audioSource.pitch = pitch;
+        }
+    }
+
+    public void SetReverbZoneMix(float reverbZoneMix) {
+        foreach (var audioSource in audioSources) {
+            audioSource.reverbZoneMix = reverbZoneMix;
+        }
+    }
+
+    public void Play() {
+        foreach (var audioSource in audioSources) {
+            audioSource.Play();
+        }
+    }
+
+    public void PlayDelayed(float delay) {
+        foreach (var audioSource in audioSources) {
+            audioSource.PlayDelayed(delay);
+        }
+    }
+
+    public void PlayOneShot(AudioClip clip, float volumeScale = 1.0f) {
+        foreach (var audioSource in audioSources) {
+            audioSource.PlayOneShot(clip, volumeScale);
+        }
+    }
+
+    public void PlayScheduled(double time) {
+        foreach (var audioSource in audioSources) {
+            audioSource.PlayScheduled(time);
+        }
+    }
+
+    public void SetScheduledStartTime(double time) {
+        foreach (var audioSource in audioSources) {
+            audioSource.SetScheduledStartTime(time);
+        }   
+    }
+
+    public void SetScheduledEndTime(double time) {
+        foreach (var audioSource in audioSources) {
+            audioSource.SetScheduledEndTime(time);
+        }
+    }
+
+    public float GetTime() {
+        return audioSources[0].time;
+    }
+
+    public void SetTime(float time) {
+        foreach (var audioSource in audioSources) {
+            audioSource.time = time;
+        }
+    }
+
+    public int GetTimeSamples() {
+        return audioSources[0].timeSamples;
+    }
+
+    public void SetTimeSamples(int timeSamples) {
+        foreach (var audioSource in audioSources) {
+            audioSource.timeSamples = timeSamples;
+        }
+    }
+
+    public void Stop() {
+        foreach (var audioSource in audioSources) {
+            audioSource.Stop();
+        }
+    }
+
+    public void Pause() {
+        foreach (var audioSource in audioSources) {
+            audioSource.Pause();
+        }
+    }
+
+    public void UnPause() {
+        foreach (var audioSource in audioSources) {
+            audioSource.UnPause();
+        }
+    }
+
+    public bool GetIsPlaying() {
+        return audioSources[0].isPlaying;
+    }
+}
